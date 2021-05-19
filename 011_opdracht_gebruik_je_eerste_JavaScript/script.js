b99d0bed972a7381bd0b879650abc463372f96f8
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-05-19 17:50:09 +0200

    Add files via upload

---
+++ 011_opdracht_gebruik_je_eerste_JavaScript/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let name = "Daniël";
+console.log(name);
+let uitkomst1 = 4 + 4;
+console.log(uitkomst1);
+let uitkomst2 = "4" + "4";
+console.log(uitkomst2);
+console.log(2 + 2);
+console.log(4 - 2);
+console.log(2 * 3);
+console.log(12 % 100);
+let age = "33";
+console.log(typeof age);
