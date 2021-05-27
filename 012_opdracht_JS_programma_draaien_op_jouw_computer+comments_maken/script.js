1f4507a9e6017509f1bca3c7ddcc0dd60bdb364e
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-05-27 20:58:27 +0200

    Add files via upload

---
+++ 012_opdracht_JS_programma_draaien_op_jouw_computer+comments_maken/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som
+/*let uitkomst1 = 1230941 * 1823794;
+console.log(uitkomst1);
+let uitkomst2 = 637263 / 54;
+console.log(uitkomst2);
+*/
