052d994c351aac9ce8c24719e8e267ff959f9a1f
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-05-28 11:38:15 +0200

    Add files via upload

--- 012_opdracht_JS_programma_draaien_op_jouw_computer+comments_maken/script.js
+++ 012_opdracht_JS_programma_draaien_op_jouw_computer+comments_maken/script.js
@@ -5,3 +5,17 @@ console.log(uitkomst1);
 let uitkomst2 = 637263 / 54;
 console.log(uitkomst2);
 */
+// Dit is een grote som
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* Ik ben een blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+// Code die niet "aan" staat zie je niet in de console. 
+
+// Let op: runt JavaScript in Node? Dan is je terminal de console. 
+// Runt JavaScript in je browser? Dan vind je de console in je developer tools. 
