e7bee515cb2cf3bc6c9e21fdf9057976cb4d0e46
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-05-28 11:41:58 +0200

    Add files via upload

---
+++ 013_opdracht_Gebruik_if-else_statements/kroeg.js
@@ -0,0 +1,38 @@
+const age = 17;
+
+if (age > 18) {
+
+    console.log("You shall pass");
+
+} else {
+
+    console.log("You shall not pass");
+
+}
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("Welcome on Ladies Night");
+
+} else {
+
+    console.log("Not welcome on Ladies Night");
+
+}
+
+const driverStatus = "Henk";
+
+if (driverStatus == "Bob") {
+
+    console.log("Suitable to drive");
+
+} else {
+
+    console.log("Not suitable to drive");
+
+}
+
+
+
