6e5a6e8180261021e69e47cc6ff1d95774ed932d
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-06-02 14:59:02 +0200

    Add files via upload

---
+++ 016_opdracht_Functions_basics/firstFunction.js
@@ -0,0 +1,31 @@
+const makePizza = function () {
+
+    console.log('Make dough')
+    console.log('Let dough rise')
+    console.log('Roll out dough')
+    console.log('Put on pizza sauce')
+    console.log('Sprinkle on cheese')
+    console.log('Put on oregano')
+    console.log('Place pizza in oven')
+    console.log('Take out pizza')
+    console.log('Cut pizza into 8 or 10 slices')
+}
+
+const makeSushi = function () {
+
+    console.log('Boil rice')
+    console.log('Add vinegar to rice')
+    console.log('Make balls out of rice')
+    console.log('Put rice balls into nori leafs')
+    console.log('Place tuna on the rice balls')
+    console.log('Roll up the nori leaf into suchi roll')
+    console.log('Slice 2cm thick slices out of the roll')
+    console.log('Put soj sauce into bowl')
+    console.log('Add wasabi if you like')
+}
+
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
+makeSushi();
