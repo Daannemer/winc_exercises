7aad522404afa3d4f0d6b2b799ccc0138f599eef
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-06-10 21:08:52 +0200

    Add files via upload

---
+++ 021_opdracht_For_loops/even-odd-report.js
@@ -0,0 +1,9 @@
+//Even Odd Reporter
+
+for (i = 0; i <= 20; i++) {
+    if (i % 2) {
+        console.log(i + " is uneven");
+    } else {
+        console.log(i + " is even");
+    }
+};
---
+++ 021_opdracht_For_loops/multiplication-tables.js
@@ -0,0 +1,7 @@
+//Multiplication tables
+const number = 9;
+
+for (i = 0; i <= 10; i++) {
+    const result = number * i;
+    console.log(i + " x " + number + " = " + result);
+};
