9826631007002dbaa34ea8dcb4d2a5d0055daa70
Daannemer <82450573+Daannemer@users.noreply.github.com>
2021-06-11 11:46:46 +0200

    Delete 023_opdracht_guess_the_number_game directory

--- 023_opdracht_guess_the_number_game/script.js
+++
@@ -1,9 +0,0 @@
-const askName = function () {
-    var person = prompt("Please enter your name", "your name");
-    if (person != null) {
-        document.getElementById("demo").innerHTML =
-            "Hello " + person + "! How are you today?";
-    }
-}
-
-document.getElementById("demo").innerHTML = 5 + 6;
